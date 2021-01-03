cc6a7e54ef0a58b67326ba9e1f6045049e2c1350
Scott James <scottaustinjames@gmail.com>
2021-01-03 02:45:42 +0000

    Add files via upload

---
+++ second.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="google details">
+	<meta name="keywords" content="HTML, CSS, JavaScript, php">
+	<meta name="author" content="Scott James">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Greeting Cards</title>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+	<link rel=stylesheet type="text/css" href="second-style.css">
+	<script src="javascript.js"></script>
+</head>
+<body>
+	<header>
+
+		<div id="header-content">
+		<a href="#"><img src="Sass.png"></a>
+		<!-- Navigation bar -->
+		<nav id="navBar">
+    		<ul>
+        		<li><a href="default.asp">Contact</a></li>
+        		<li><a href="news.asp">Gallery</a></li>
+        		<li><a href="contact.asp">Services</a></li>
+        		<li><a href="about.asp">Home</a></li>
+   			 </ul>
+		</nav>
+		<!-- Header Text -->
+
+	<div class="header-text">
+		<h1>Hello I'm Sandra<br>
+			<div>I handcraft cards for all occasions</div>
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr,sed diam nonumy eirmod tempor invidunt ut</h1>
+		<a href="#contact">Contact</a>
+	</div>
+	</div>
+
+	</header>
+</body>
+</html>
