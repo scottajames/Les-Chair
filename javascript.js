b0a221ade445b5220a5dca50e175002e8d11864d
Scott James <scottaustinjames@gmail.com>
2021-03-05 12:46:05 +0000

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function navigation() {
-  var x = document.getElementById("navBar");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
